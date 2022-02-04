0f6d557e20d52126760b5c1885494cff0cc4621d
DANIEL-ACER\Daniel <danya.ostap@gmail.com>
2022-02-04 17:35:00 +0200

    Created a function that adapts key length to input message length

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 
 def encrypt_vigenere(input_message, key):
     return ""
+
+
+def check_key(input_message, key):
+    if len(input_message) == len(key):
+        return key
+    for i in range(len(input_message) - len(key)):
+        key += key[i % len(key)]
+    return key
+
+
+print(check_key('abcdefgh', 'distlab'))
