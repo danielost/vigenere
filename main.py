4684d3258b3768e28cc2f8a8367a0f0672d7103e
DANIEL-ACER\Daniel <danya.ostap@gmail.com>
2022-02-04 17:59:10 +0200

    Created encryption function

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Following function implements encryption with Vigenere Cypher
-
-def encrypt_vigenere(input_message, key):
-    return ""
-
-
 def check_key(input_message, key):
     if len(input_message) == len(key):
         return key
@@ -12,4 +6,24 @@ def check_key(input_message, key):
     return key
 
 
-print(check_key('abcdefgh', 'distlab'))
+# encryption vvv
+
+
+def encrypt_vigenere(input_message, key):
+    key = check_key(input_message, key)
+    output = ''
+    for i in range(len(input_message)):
+        if input_message[i] == ' ':
+            output += ' '
+            continue
+        temp = (ord(input_message[i]) + ord(key[i])) % 26
+        temp += ord('A')
+        output += chr(temp)
+    return output
+
+
+print(encrypt_vigenere('HELLO THERE', 'DISTLAB'))
+
+
+
+
