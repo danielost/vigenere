12c6faa4bdaffee8b63d3c8ca6687111ce66027d
DANIEL-ACER\Daniel <danya.ostap@gmail.com>
2022-02-04 18:42:09 +0200

    Added a function to check if the input data contains only uppercase letters

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def check_key(input_message, key):
 
 
 def encrypt_vigenere(input_message, key):
+    if not check_case(input_message, key):
+        return 'Enter the message and the key using only upper case'
     key = check_key(input_message, key)
     output = ''
     for i in range(len(input_message)):
@@ -26,6 +28,8 @@ def encrypt_vigenere(input_message, key):
 
 
 def decrypt_vigenere(input_message, key):
+    if not check_case(input_message, key):
+        return 'Enter the message and the key using only upper case'
     key = check_key(input_message, key)
     output = ''
     for i in range(len(input_message)):
@@ -36,6 +40,16 @@ def decrypt_vigenere(input_message, key):
         temp += ord('A')
         output += chr(temp)
     return output
+
+
+def check_case(input_message, key):
+    for i in range(len(input_message)):
+        if input_message[i].islower():
+            return False
+    for i in range(len(key)):
+        if input_message[i].islower():
+            return False
+    return True
 
 
 print(encrypt_vigenere('HELLO THERE', 'DISTLAB'))
